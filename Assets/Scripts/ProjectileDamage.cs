94573ff08c1d49a88e6bce2846a4e71e44d7b743
RaoulEneskjoldLBS <raoul.eneskjold@elev.ga.lbs.se>
2025-01-14 16:18:18 +0100

    Trying to make projectile work

---
+++ Assets/Scripts/ProjectileDamage.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileDamage : MonoBehaviour
+{
+
+    PlayerHealth playerHealth;
+
+    [SerializeField]
+    GameObject gameObjectt;
+
+   //public float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      // time = Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision");
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerHealth.TakeDamage(20);
+          
+            Debug.Log("20 damage");
+            Destroy(gameObject);
+        }
+        
+        
+            Destroy(gameObject);
+        
+    }
+}
