6b0dfb837824783004ddcbd54a988ba2b6d03bf8
LowTierJack <151172501+LowTierJack@users.noreply.github.com>
2025-01-15 15:59:40 +0100

    K.BWEBLJESJF

--- Assets/Scripts/BossAttackLogic.cs
+++ Assets/Scripts/BossAttackLogic.cs
@@ -13,43 +13,67 @@ public class BossAttackLogic : MonoBehaviour
     Transform player;
     Rigidbody2D rb;
     Enemy enemy;
+    bool attacking = false;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     public void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        animator = GetComponent<Animator>();
         rb = animator.GetComponent<Rigidbody2D>();
         enemy = animator.GetComponent<Enemy>();
+
+    }
+
+    IEnumerator vineRoutine()
+    {
+        animator.SetTrigger("Vine");
+        GameObject projectilespawn = Instantiate(vine, player.position + new Vector3(0, -1.5f, 0), Quaternion.identity);
+        yield return new WaitForSeconds(0.900F);
+        attacking = false;
+        animator.ResetTrigger("Vine");
+        
+    }
+
+    IEnumerator vineSideRoutine()
+    {
+        animator.SetTrigger("SideVine");
+        GameObject projectilespawn = Instantiate(vineSide, player.position + new Vector3(0, -1.5f, 0), Quaternion.identity);
+        yield return new WaitForSeconds(0.900F);
+        attacking = false;
+        animator.ResetTrigger("SideVine");
 
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     public void Update()
     {
+        if (attacking == false)
+        {
 
-      
-            int chooseattack = Random.Range(3, 0);
+            int chooseattack = Random.Range(1, 4);
 
             if (chooseattack == 1)
             {
-                animator.SetTrigger("Vine");
-                GameObject projectilespawn = Instantiate(vine, player.position, Quaternion.identity);
+                StartCoroutine(vineRoutine());
+                attacking = true;
             }
 
             if (chooseattack == 2)
             {
-                animator.SetTrigger("VineWave");
+                StartCoroutine(vineRoutine());
+                attacking = true;
             }
 
 
             if (chooseattack == 3)
             {
-                animator.SetTrigger("SideVine");
-                GameObject projectilespawn = Instantiate(vineSide, player.position, Quaternion.identity);
+                StartCoroutine(vineSideRoutine());
+                attacking = true;
             }
 
-        
-    }
 
+        }
+    }
   
 }
