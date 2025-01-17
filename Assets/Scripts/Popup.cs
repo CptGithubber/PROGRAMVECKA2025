a7bbba785a68061bee3cd8958800cfc1e16a05c6
RaoulEneskjoldLBS <raoul.eneskjold@elev.ga.lbs.se>
2025-01-17 12:51:11 +0100

    comment

--- Assets/Scripts/Popup.cs
+++ Assets/Scripts/Popup.cs
@@ -12,6 +12,7 @@ public class Popup : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Gets component.
         animator = GetComponent<Animator>();
        
     }
